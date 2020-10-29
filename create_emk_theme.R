ea9931dd436b3b953451f2d27ecd4b2600b0706a
Ella Kaye <ella.kaye@gmail.com>
2020-10-29 06:42:57 +0000

    Lots of tweaks

--- create_emk_theme.R
+++ create_emk_theme.R
@@ -3,24 +3,29 @@ library(xaringanthemer)
 emk_colours <- list(
   pink = "#D40067",
   blue = "#0079D1",
-  pale1 = "#FDFCFA",
-  pale2 = "#FDFBF0",
-  dark1 = "#002B36",
-  dark2 = "#073642",
+  pale = "#FDFCFA",
+  dark = "#002B36",
   grey = "#586E75"
 )
 
 style_xaringan(
-  background_color = emk_colours$pale1,
+  background_color = emk_colours$pale,
   link_color = emk_colours$blue,
   header_color = emk_colours$pink,
   inverse_background_color = emk_colours$blue,
   text_font_google = google_font("Montserrat"),
   code_font_google = google_font("Fira Code"),
-  inverse_text_color = emk_colours$pale1,
-  text_color = emk_colours$dark1,
-  #text_bold_color = emk_colours$dark1,
+  inverse_text_color = emk_colours$pale,
+  text_color = emk_colours$dark,
+  text_bold_color = emk_colours$pink,
   code_inline_color = emk_colours$grey,
   footnote_color = emk_colours$pink,
-  text_slide_number_color = emk_colours$grey
+  text_slide_number_color = emk_colours$grey,
+  colors = c(
+    pink = "#D40067",
+    blue = "#0079D1",
+    pale = "#FDFCFA",
+    dark = "#002B36",
+    grey = "#586E75"
+  )
 )
